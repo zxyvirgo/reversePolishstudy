70a15236bd1116fe7b69a1d63164908d2a66573b
zxyvirgo <zxyvirgo@aliyun.com>
2014-07-02 13:40:39 +0800

    format

--- expression/src/expression/Calculate.java
+++ expression/src/expression/Calculate.java
@@ -1,47 +1,48 @@
 package expression;
 
-public class Calculate { 
-// 判断是否为操作符号 
-public static boolean isOperator(String operator) { 
-if (operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("/") 
-|| operator.equals("(") || operator.equals(")")) 
-return true; 
-else 
-return false; 
-} 
+public class Calculate {
+	// 判断是否为操作符号
+	public static boolean isOperator(String operator) {
+		if (operator.equals("+") || operator.equals("-")
+				|| operator.equals("*") || operator.equals("/")
+				|| operator.equals("(") || operator.equals(")"))
+			return true;
+		else
+			return false;
+	}
 
-// 设置操作符号的优先级别 
-public static int priority(String operator) { 
-if (operator.equals("+") || operator.equals("-")) 
-return 1; 
-else if (operator.equals("*") || operator.equals("/")) 
-return 2; 
-else 
-return 0; 
-} 
+	// 设置操作符号的优先级别
+	public static int priority(String operator) {
+		if (operator.equals("+") || operator.equals("-"))
+			return 1;
+		else if (operator.equals("*") || operator.equals("/"))
+			return 2;
+		else
+			return 0;
+	}
 
-// 做2值之间的计算 
-public static String twoResult(String operator, String a, String b) { 
-try { 
-String op = operator; 
-String rs = new String(); 
-double x = Double.parseDouble(b); 
-double y = Double.parseDouble(a); 
-double z = 0; 
-if (op.equals("+")) 
-z = x + y; 
-else if (op.equals("-")) 
-z = x - y; 
-else if (op.equals("*")) 
-z = x * y; 
-else if (op.equals("/")) 
-z = x / y; 
-else 
-z = 0; 
-return rs + z; 
-} catch (NumberFormatException e) { 
-System.out.println("input has something wrong!"); 
-return "Error"; 
-} 
-} 
+	// 做2值之间的计算
+	public static String twoResult(String operator, String a, String b) {
+		try {
+			String op = operator;
+			String rs = new String();
+			double x = Double.parseDouble(b);
+			double y = Double.parseDouble(a);
+			double z = 0;
+			if (op.equals("+"))
+				z = x + y;
+			else if (op.equals("-"))
+				z = x - y;
+			else if (op.equals("*"))
+				z = x * y;
+			else if (op.equals("/"))
+				z = x / y;
+			else
+				z = 0;
+			return rs + z;
+		} catch (NumberFormatException e) {
+			System.out.println("input has something wrong!");
+			return "Error";
+		}
+	}
 }
--- expression/src/expression/Stacks.java
+++ expression/src/expression/Stacks.java
@@ -1,24 +1,25 @@
 package expression;
+
 import java.util.LinkedList;
 
-public class Stacks { 
-private LinkedList list = new LinkedList(); 
-int top = -1; 
+public class Stacks {
+	private LinkedList list = new LinkedList();
+	int top = -1;
 
-public void push(Object value) { 
-top++; 
-list.addFirst(value); 
-} 
+	public void push(Object value) {
+		top++;
+		list.addFirst(value);
+	}
 
-public Object pop() { 
-Object temp = list.getFirst(); 
-top--; 
-list.removeFirst(); 
-return temp; 
+	public Object pop() {
+		Object temp = list.getFirst();
+		top--;
+		list.removeFirst();
+		return temp;
 
-} 
+	}
 
-public Object top() { 
-return list.getFirst(); 
-} 
-} 
+	public Object top() {
+		return list.getFirst();
+	}
+}
